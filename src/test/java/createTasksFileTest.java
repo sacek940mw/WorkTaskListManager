19a5654ac77f5a54f6c51acac0f16d993c6695be
sacek940mw <plsacekpl@gmail.com>
2023-02-27 13:30:21 +0100

    27.02.22 -3

--- src/test/java/createTasksFileTest.java
+++ src/test/java/createTasksFileTest.java
@@ -1,10 +1,11 @@
 import org.example.fileOperations.SaveTasks;
+import org.example.tasks.AllTasksList;
 import org.junit.jupiter.api.Test;
 
 public class createTasksFileTest {
     @Test
-    void createTasksFileTest() throws InterruptedException {
-        Thread st = new Thread(new SaveTasks());
+    void createTasksFileTestMethod() throws InterruptedException {
+        Thread st = new Thread(new SaveTasks("files\\tasks.ser", AllTasksList.getInstance()));
         st.start();
         st.join();
     }
